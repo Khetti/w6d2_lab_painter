4fe10b5047af990d379056cd9cb2dcf6a6e84bac
Greg Shanks <greg.shanks@gmail.com>
2019-12-10 16:33:43 +0000

    added initial test function to decorator_spec.js; test passed

--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,2 +1,17 @@
 const assert = require("assert");
 const Decorator = require("../decorator.js")
+
+describe("Decorator", function(){
+
+  let decorator;
+
+  beforeEach(function(){
+    decorator = new Decorator([]);
+  });
+
+  it("should start with an empty paint stock", function(){
+    const actual = decorator.paintStock.length;
+    assert.strictEqual(actual, 0)
+  });
+
+});
