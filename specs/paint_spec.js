fefd192c914a09974bb1eeca180ad272520e9bef
Greg Shanks <greg.shanks@gmail.com>
2019-12-10 16:24:25 +0000

    paint_spec tests create and passing

--- specs/paint_spec.js
+++ specs/paint_spec.js
@@ -5,6 +5,24 @@ describe("Paint", function(){
 
   let paint;
 
-  beforeEach(function())
+  beforeEach(function(){
+    paint = new Paint(10, false);
+  });
 
-})
+  it("should have a number of litres of paint", function(){
+    const actual = paint.amount;
+    assert.strictEqual(actual, 10);
+  });
+
+  it("should be able to check if it is empty", function(){
+    const actual = paint.isEmpty;
+    assert.strictEqual(actual, false);
+  });
+
+  it("should be able to empty itself of paint", function(){
+    paint.amount = 0;
+    const actual = paint.amount;
+    assert.strictEqual(actual, 0);
+  });
+
+});
